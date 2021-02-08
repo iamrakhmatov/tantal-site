f97f22497d268a5059f18269b586c87b527b582f
Shakhruz <i.rakhmatov97@gmail.com>
2021-02-09 04:04:32 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="bg-indigo-100 h-screen">
+      <header className="flex flex-col items-center h-full justify-center">
+        <img src={logo} className="w-64" alt="logo" />
+        <p className="text-lg text-indigo-500 font-semibold">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-indigo-800 border border-indigo-500 bg-indigo-200 rounded-md py-1 px-3 mt-2"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
